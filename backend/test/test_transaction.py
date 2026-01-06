c052ce441c312d1c9e210417cb447b94f95d8c49
Wylwaryn <marcinmatthieu@gmail.com>
2026-01-06 12:03:13 +0100

    first test - RED

--- backend/test/test_transaction.py
+++ backend/test/test_transaction.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from backend.src.transaction import Transaction
+from src.transaction import Transaction
 from datetime import date
 from decimal import Decimal
 
