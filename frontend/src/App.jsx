4cd1ef083137db367f0f8a19cd5beca75533f073
Romain-mont <romainmontagnon26200@gmail.com>
2026-01-18 09:53:44 +0100

    initialization front + react + tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 border-4 border-blue-600 p-4 rounded-lg">
+        Tailwind fonctionne ! 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
